6214f4c40e0c156ff7f1d3a13310831f9f559db2
Aviciena Santoso <avi.santoso@gmail.com>
2021-02-11 10:22:54 +0800

    add basic onclick functionality and structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import * as _ from "lodash";
+import { makeStyles, Button } from "@material-ui/core";
+
 import buttons from "./buttons.json";
-import "./styles.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  app: {
+    background: theme.palette.primary.light,
+    height: 600,
+    width: 500,
+    margin: "auto"
+  }
+}));
 
 export default function App() {
-  const operators = _.filter(buttons, (x) => x.type === "operator");
-  const digits = _.filter(buttons, (x) => x.type === "digit");
+  const styles = useStyles();
 
-  console.log("operators", operators);
-  console.log("digits", digits);
+  const handleClick = (e) => {
+    console.log(e.currentTarget.id);
+  };
 
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className={styles.root}>
+      <div className={styles.app}>
+        <ButtonsList buttons={buttons} handleClick={handleClick}></ButtonsList>
+      </div>
     </div>
   );
 }
+
+const ButtonsList = ({ buttons, handleClick }) => {
+  return (
+    <div>
+      {buttons.map((x) => (
+        <Button variant="outlined" id={x.id} key={x.id} onClick={handleClick}>
+          {x.display}
+        </Button>
+      ))}
+    </div>
+  );
+};

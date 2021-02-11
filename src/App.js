300b5125afde0bf21ecfcacae250bc2e9d999223
Aviciena Santoso <avi.santoso@gmail.com>
2021-02-11 10:05:27 +0800

    get operators and digits from buttons array

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import buttons from "./buttons.json";
 import "./styles.css";
 
 export default function App() {
+  const operators = _.filter(buttons, (x) => x.type === "operator");
+  const digits = _.filter(buttons, (x) => x.type === "digit");
+
+  console.log("operators", operators);
+  console.log("digits", digits);
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
